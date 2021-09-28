800afcac7d650c0e9f8233863f50753af46498c8
Forever <1874861374@qq.com>
2021-09-28 08:34:16 +0800

    fixed: babel final

--- ES6/1-babel/src/app.js
+++ ES6/1-babel/src/app.js
@@ -1,7 +1,11 @@
 var arr = [];
 
-for (let i = 0; i < 10; i++){
-    arr[i] = (() => {
-        console.log(i);
-    })(1);
+for (let i = 0; i < 10; i++) {
+  arr[i] = (() => {
+    console.log(i);
+  })(1);
+}
+
+function getSum(nums) {
+  return nums.reduce((total, cur) => total + cur, 0);
 }
