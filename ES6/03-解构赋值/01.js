6f153d421782b7730b42acdf6d6ce22c6d597ad9
Forever <1874861374@qq.com>
2021-10-08 08:22:56 +0800

    add: 03-解构赋值

---
+++ ES6/03-解构赋值/01.js
@@ -0,0 +1,51 @@
+// var x = 1;
+// {
+//   let x = x;
+//   console.log(x);
+// }
+
+// function foo(x = 1, y =2){
+//     console.log(x + y);
+// }
+
+// let x = 1;
+// function foo(y = x) {
+//   let x = 2;
+//   console.log(y);
+// }
+// foo();
+
+// let x = 1;
+// function foo(x = 2) {
+//   let x = 2;
+//   console.log(x);
+// }
+// foo();
+
+// let x = 1;
+// function foo(x = x) {
+//   //相当于let x = x
+//   console.log(x);
+// }
+// foo();
+
+// var person = {
+//   name: "zhangsan",
+//   age: 50,
+//   son: {
+//     name: "lisi",
+//     age: 30,
+//     son: {
+//       name: "wangwu",
+//       age: 12,
+//     },
+//   },
+// };
+
+// let {
+//   son: {
+//     son: { name: name },
+//   },
+// } = person;
+// console.log(name);
+
