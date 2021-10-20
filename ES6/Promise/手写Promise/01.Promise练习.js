8e05394370ed1cebeba0d5dd7078b1acc3caa2dc
Forever <1874861374@qq.com>
2021-10-20 21:13:09 +0800

    add: 面试/day01

--- ES6/Promise/手写Promise/01.Promise练习.js
+++ ES6/Promise/手写Promise/01.Promise练习.js
@@ -1,12 +1,10 @@
-// 1. Promise的几道基础题
-// 1.1
+// 1.1  promise1 1 Promise{<pending>}
 // const promise1 = new Promise((resolve, reject) => {
-//   console.log("promise1"); //promise1
+//   console.log("promise1");
 // });
-// console.log("1", promise1); // 1 promise{<pending>}
+// console.log("1", promise1);
 
-// 1.2
-// 1 2 4 3(同步和异步)
+// 1.2  1 2 4 3
 // const promise = new Promise((resolve, reject) => {
 //   console.log(1);
 //   resolve("success");
@@ -17,8 +15,7 @@
 // });
 // console.log(4);
 
-// 1.3
-// 1 2 4
+// 1.3  1 2 4
 // const promise = new Promise((resolve, reject) => {
 //   console.log(1);
 //   console.log(2);
@@ -28,8 +25,7 @@
 // });
 // console.log(4);
 
-// 1.4
-// promise1 1 promise{<fulfilled>} 2 promise{<pending>} resolve1
+// 1.4  promise1  1 Promise{<fulfilled>} 2 Promise{<pending>} resolve1
 // const promise1 = new Promise((resolve, reject) => {
 //   console.log("promise1");
 //   resolve("resolve1");
@@ -40,8 +36,7 @@
 // console.log("1", promise1);
 // console.log("2", promise2);
 
-// 1.5
-// 1 start success
+// 1.5  start 1 success(fn先执行)
 // const fn = () =>
 //   new Promise((resolve, reject) => {
 //     console.log(1);
@@ -52,8 +47,7 @@
 // });
 // console.log("start");
 
-// 1.6
-// start 1 success
+// 1.6  start 1 success
 // const fn = () =>
 //   new Promise((resolve, reject) => {
 //     console.log(1);
@@ -64,9 +58,7 @@
 //   console.log(res);
 // });
 
-// 2. Promise结合setTimeout
-// 2.1
-// start end resolve time
+// 2.1  start   end resolve time
 // console.log("start");
 // setTimeout(() => {
 //   console.log("time");
@@ -76,8 +68,7 @@
 // });
 // console.log("end");
 
-// 2.2
-// 1 2 4 timerStart timerEnd success
+// 2.2  1 2 4 timerStart timerEnd success
 // const promise = new Promise((resolve, reject) => {
 //   console.log(1);
 //   setTimeout(() => {
@@ -92,8 +83,7 @@
 // });
 // console.log(4);
 
-// 2.3
-// start timer1 timer2 timer3
+// 2.3(1)   start timer1 timer2 timer3
 // setTimeout(() => {
 //   console.log("timer1");
 //   setTimeout(() => {
@@ -105,7 +95,7 @@
 // }, 0);
 // console.log("start");
 
-// start timer1 promise timer2
+// 2.3(2)   start timer1 promise timer2
 // setTimeout(() => {
 //   console.log("timer1");
 //   Promise.resolve().then(() => {
@@ -117,8 +107,7 @@
 // }, 0);
 // console.log("start");
 
-// 2.4
-// start promise1 timer1 promise2 timer2
+// 2.3  start promise1 timer1 promise2 timer2
 // Promise.resolve().then(() => {
 //   console.log("promise1");
 //   const timer2 = setTimeout(() => {
@@ -133,8 +122,7 @@
 // }, 0);
 // console.log("start");
 
-// 2.5
-// promise1, Promise{<pending>} promise2 Promise{<penging>} error!! promise1 Promise{<resolved>: "success"} promise2 Promise{<rejected>: "Error: error!!"}
+// 2.4  promise1 Promise{<pending>} promise2 Promise{<pending>} Errpr:error!!! promise1 Promise{<fulfilled>:'success'} promise2 Promise{<rejected>: Error:error!!!}
 // const promise1 = new Promise((resolve, reject) => {
 //   setTimeout(() => {
 //     resolve("success");
@@ -150,8 +138,7 @@
 //   console.log("promise2", promise2);
 // }, 2000);
 
-// 2.6
-// promise1里的内容 promise1 Promise{<pending>} promise2 Promise{<pending>} timer1 Error timer2 promise1 Promise{<resolved>: "success"} promise2 Promise{<reject>: "Error: error!!"}
+// 2.5  promise1里的内容 promise1 Promise{<pending>} promise2 Promise{<pending>} timer1 Error:error!!! timer2 promise1: Promise{<fulfilled>: success} promise2 Promise{<reject>: Error:error!!!}
 // const promise1 = new Promise((resolve, reject) => {
 //   setTimeout(() => {
 //     resolve("success");
@@ -170,4 +157,263 @@
 //   console.log("promise2", promise2);
 // }, 2000);
 
-// 3.Promise中的then、catch、finally
+// 3.1  then, success1
+// const promise = new Promise((resolve, reject) => {
+//   resolve("success1");
+//   reject("error");
+//   resolve("success2");
+// });
+// promise
+//   .then((res) => {
+//     console.log("then: ", res);
+//   })
+//   .catch((err) => {
+//     console.log("catch: ", err);
+//   });
+
+// 3.2  catch error then3 undefined
+// const promise = new Promise((resolve, reject) => {
+//   reject("error");
+//   resolve("success2");
+// });
+// promise
+//   .then((res) => {
+//     console.log("then1: ", res);
+//   })
+//   .then((res) => {
+//     console.log("then2: ", res);
+//   })
+//   .catch((err) => {
+//     console.log("catch: ", err);
+//   })
+//   .then((res) => {
+//     console.log("then3: ", res);
+//   });
+
+// 3.3  1 2
+// Promise.resolve(1)
+//   .then((res) => {
+//     console.log(res);
+//     return 2;
+//   })
+//   .catch((err) => {
+//     return 3;
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+
+// 3.4  1 3
+// Promise.reject(1)
+//   .then((res) => {
+//     console.log(res);
+//     return 2;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     return 3;
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+
+// 3.5  timer success 1001 success 1001
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("timer");
+//     resolve("success");
+//   }, 1000);
+// });
+// const start = Date.now();
+// promise.then((res) => {
+//   console.log(res, Date.now() - start);
+// });
+// promise.then((res) => {
+//   console.log(res, Date.now() - start);
+// });
+
+// 3.6  Error:error!!!(没有这个) then: Error: error!!!
+// Promise.resolve()
+//   .then(() => {
+//     return new Error("error!!!");
+//   })
+//   .then((res) => {
+//     console.log("then: ", res);
+//   })
+//   .catch((err) => {
+//     console.log("catch: ", err);
+//   });
+
+// 3.7  .then()返回的不能是promise对象本身，会报错
+// const promise = Promise.resolve().then(() => {
+//   return promise;
+// });
+// promise.catch(console.err);
+
+// 3.8  then或者catch传入非函数会发生值穿透（1）
+// Promise.resolve(1).then(2).then(Promise.resolve(3)).then(console.log);
+
+// 3.9(1)  error: err!!!
+// Promise.reject("err!!!")
+//   .then(
+//     (res) => {
+//       console.log("success", res);
+//     },
+//     (err) => {
+//       console.log("error", err);
+//     }
+//   )
+//   .catch((err) => {
+//     console.log("catch", err);
+//   });
+
+// 3.9(2)   catch error!!!
+// Promise.reject("error!!!")
+//   .then((res) => {
+//     console.log("success", res);
+//   })
+//   .catch((err) => {
+//     console.log("catch", err);
+//   });
+
+// 3.9(3)   fail2 Error:error!!!
+// Promise.resolve()
+//   .then(
+//     function success(res) {
+//       throw new Error("error!!!");
+//     },
+//     function fail1(err) {
+//       console.log("fail1", err);
+//     }
+//   )
+//   .catch(function fail2(err) {
+//     console.log("fail2", err);
+//   });
+
+// 3.10(1) 1 finally2 finally finally2后面的then函数 我是finally2返回的值(2错了，除非报错才返回异常的值)
+// Promise.resolve("1")
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .finally(() => {
+//     console.log("finally");
+//   });
+// Promise.resolve("2")
+//   .finally(() => {
+//     console.log("finally2");
+//     return "我是finally2返回的值";
+//   })
+//   .then((res) => {
+//     console.log("finally2后面的then函数", res);
+//   });
+
+// 3.10(2)  finally1 捕获错误 Error: 我是finally中抛出的异常
+// Promise.resolve("1")
+//   .finally(() => {
+//     console.log("finally1");
+//     throw new Error("我是finally中抛出的异常");
+//   })
+//   .then((res) => {
+//     console.log("finally后面的then函数", res);
+//   })
+//   .catch((err) => {
+//     console.log("捕获错误", err);
+//   });
+
+// 3.10(3)  promise1  1 error1  finally1 finally2
+// function promise1() {
+//   let p = new Promise((resolve) => {
+//     console.log("promise1");
+//     resolve("1");
+//   });
+//   return p;
+// }
+// function promise2() {
+//   return new Promise((resolve, reject) => {
+//     reject("error");
+//   });
+// }
+// promise1()
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err))
+//   .finally(() => console.log("finally1"));
+
+// promise2()
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err))
+//   .finally(() => console.log("finally2"));
+
+// 3.10(4)  promise1 1 error finally1 finally2
+// function promise1() {
+//   let p = new Promise((resolve) => {
+//     console.log("promise1");
+//     resolve("1");
+//   });
+//   return p;
+// }
+// function promise2() {
+//   return new Promise((resolve, reject) => {
+//     reject("error");
+//   });
+// }
+// promise1()
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err))
+//   .then(() => console.log("finally1"));
+
+// promise2()
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err))
+//   .then(() => console.log("finally2"));
+
+// 4.1
+// function runAsync(x) {
+//   const p = new Promise((resolve) => {
+//     return setTimeout(() => {
+//       return resolve(x, console.log(x));
+//     }, 1000);
+//   });
+//   return p;
+// }
+// Promise.all([runAsync(1), runAsync(2), runAsync(3)]).then((res) =>
+//   console.log(res)
+// );
+
+// 4.2 cach会捕获最新的异常  1 3 2 Error: 2 4
+// function runAsync(x) {
+//   const p = new Promise((r) => setTimeout(() => r(x, console.log(x)), 1000));
+//   return p;
+// }
+// function runReject(x) {
+//   const p = new Promise((res, rej) =>
+//     setTimeout(() => rej(`Error: ${x}`, console.log(x)), 1000 * x)
+//   );
+//   return p;
+// }
+// Promise.all([runAsync(1), runReject(4), runAsync(3), runReject(2)])
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+
+// 4.3  1 result: 1 2 3
+// function runAsync(x) {
+//   const p = new Promise((r) => setTimeout(() => r(x, console.log(x)), 1000));
+//   return p;
+// }
+// Promise.race([runAsync(1), runAsync(2), runAsync(3)])
+//   .then((res) => console.log("result: ", res))
+//   .catch((err) => console.log(err));
+
+// 4.4
+function runAsync(x) {
+  const p = new Promise((r) => setTimeout(() => r(x, console.log(x)), 1000));
+  return p;
+}
+function runReject(x) {
+  const p = new Promise((res, rej) =>
+    setTimeout(() => rej(`Error: ${x}`, console.log(x)), 1000 * x)
+  );
+  return p;
+}
+Promise.race([runReject(0), runAsync(1), runAsync(2), runAsync(3)])
+  .then((res) => console.log("result: ", res))
+  .catch((err) => console.log(err));
