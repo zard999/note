136f5fda83480325d6b61fa1cf79f4e0c0ae97b7
Forever <1874861374@qq.com>
2021-10-24 20:34:53 +0800

    add: vim

--- 面试/Promise面试题.js
+++ 面试/Promise面试题.js
@@ -25,50 +25,66 @@
 
 // 2. 使用Promise实现红绿灯交替重复亮
 // 红灯3秒亮一次，黄灯2秒亮一次，绿灯1秒亮一次；如何让三个灯不断交替重复亮灯？（用Promise实现）三个亮灯函数已经存在：
-function red() {
-  console.log("red");
-}
-function green() {
-  console.log("green");
-}
-function yellow() {
-  console.log("yellow");
-}
-
-const obj = new Map([
-  ["red", 3000],
-  ["green", 2000],
-  ["yellow", 1000],
-]);
-
-const obj1 = {
-  red: 3000,
-  green: 2000,
-  yellow: 1000,
-};
-// console.log(Object.keys(obj1));
-
-test();
-setInterval(() => {
-  test();
-}, 6000);
-
-async function test() {
-  for (let key of obj.entries()) {
-    await new Promise((r) => {
-      setTimeout(() => r(eval(key[0] + "()")), key[1]);
-    });
-  }
-}
-
-// function test() {
-//   arr.reduce((p, c) => {
-//     return p.then(() => {
-//       return new Promise((r) => {
-//         setTimeout(() => {
-//           r(red());
-//         }, c);
-//       });
-//     });
-//   }, Promise.resolve());
+// function red() {
+//   console.log("red");
 // }
+// function green() {
+//   console.log("green");
+// }
+// function yellow() {
+//   console.log("yellow");
+// }
+
+// const obj = new Map([
+//   ["red", 3000],
+//   ["green", 2000],
+//   ["yellow", 1000],
+// ]);
+
+// const obj1 = {
+//   red: 3000,
+//   green: 2000,
+//   yellow: 1000,
+// };
+
+// test();
+// setInterval(() => {
+//   test();
+// }, 6000);
+
+// async function test() {
+//   for (let key of obj.entries()) {
+//     await new Promise((r) => {
+//       setTimeout(() => r(eval(key[0] + "()")), key[1]);
+//     });
+//   }
+// }
+
+// const light = function (timer, cb) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       cb();
+//       resolve();
+//     }, timer);
+//   });
+// };
+// const step = function () {
+//   Promise.resolve()
+//     .then(() => {
+//       return light(3000, red);
+//     })
+//     .then(() => {
+//       return light(2000, green);
+//     })
+//     .then(() => {
+//       return light(1000, yellow);
+//     })
+//     .then(() => {
+//       return step();
+//     });
+// };
+
+// step();
+
+// 实现mergePromise函数，把传进去的数组按顺序先后执行，并且把返回的数据先后放到数组data中。
+func
---
+++ 面试/day04(reduce的用法)/reduce.js
@@ -0,0 +1,12 @@
+// 1.代替reverse
+// function Reverse(arr = []) {
+//   // (a,b)：S逗号运算符，最后返回b
+//   return arr.reduceRight((p, c) => (p.push(c), p), []);
+// }
+
+// console.log(Reverse([1, 2, 3, 4]));
+
+// 代替map和filter
+
+// 2.代替map和filter
+
