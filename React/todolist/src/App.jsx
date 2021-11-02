75c63ad197f5356a095cbe521d4c9087a815d796
Forever <1874861374@qq.com>
2021-11-02 18:04:58 +0800

    update: React/todolis

--- React/todolist/src/App.jsx
+++ React/todolist/src/App.jsx
@@ -1,31 +1,9 @@
 import { Component } from "react";
-import Search from "./components/Search";
-import List from "./components/List";
 
 class App extends Component {
-  state = {
-    items: [],
-    isFirst: true, //保存用户是否第一次请求
-    isLoading: false, //是否在加载中
-    errMsg: "", //错误信息
-  };
   render() {
-    const { items } = this.state;
-    return (
-      <div>
-        <div className="container">
-          <Search getUserInfo={this.getUserInfo} />
-          <List userInfo={items} state={this.state} />
-        </div>
-      </div>
-    );
+    return <div></div>;
   }
-
-  getUserInfo = (info) => {
-    this.setState({
-      ...info,
-    });
-  };
 }
 
 export default App;
