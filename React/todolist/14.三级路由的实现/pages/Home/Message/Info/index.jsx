20b4837dccf116e2443064b9aa4e021243c1aa4a
Forever <1874861374@qq.com>
2021-11-03 15:28:05 +0800

    add: React/todolist/14.三级路由的实现

---
+++ React/todolist/14.三级路由的实现/pages/Home/Message/Info/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Info extends Component {
+  render() {
+    return (
+      <ul>
+        <li>姓名：XXX </li>
+        <li>年龄：XXX </li>
+        <li>说：XXX </li>
+      </ul>
+    );
+  }
+}
